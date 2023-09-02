2bdd189a78f4013294308bff4268491d91884481
Manya750 <136895239+Manya750@users.noreply.github.com>
2023-09-02 22:53:52 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,12 @@
+package string;
+
+public class String1 {
+	public static void main(String [] args) {
+		String s1= "welcome";	
+	System.out.println(s1);
+		
+		for(int i=s1.length()-1;i>=0;i--) {
+	System.out.println(s1.charAt(i));
+	}
+}
+}
