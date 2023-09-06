fcdc37b21a166c59b11129deb6fb400895cb5e07
Manya750 <136895239+Manya750@users.noreply.github.com>
2023-09-06 22:25:31 +0530

    Add files via upload

---
+++ UpperLowerCast.java
@@ -0,0 +1,28 @@
+package string;
+
+public class String11
+{
+	 
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int count=0;
+		
+		String str="ashtfirgullaallahsemangathamaafi";
+
+      for(int i=0;i<str.length()-1;i++)
+      {
+    	  for(int j=0;j<str.length();j++)
+    	  {
+    		  if(str.charAt(i)==str.charAt(j))
+    			  	count++;
+
+    			  System.out.println(str.charAt(i) + " Count is : "+count);
+    		 
+    	  }
+
+		
+      }
+	}
+
+}
