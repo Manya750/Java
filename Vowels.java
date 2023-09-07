2185184e97c0c34ef38b669fe19832ac89c46dce
Manya750 <136895239+Manya750@users.noreply.github.com>
2023-09-07 20:18:44 +0530

    Update Vowels.java

--- Vowels.java
+++ Vowels.java
@@ -3,7 +3,7 @@ package string;
 public class Vowels {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int count=0;
 		// TODO Auto-generated method stub
 		String str = "Chinnaswamimuttuswamivenkteshwarayiyer";
